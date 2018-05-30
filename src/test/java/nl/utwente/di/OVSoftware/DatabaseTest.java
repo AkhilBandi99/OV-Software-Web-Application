1405c12262c0dd12b9ef26f733b27b05c159fc31
leonkukuk <gigalitelk@gmail.com>
2018-05-30 10:16:07 +0200

    Add DatabaseTest

---
+++ src/test/java/nl/utwente/di/OVSoftware/DatabaseTest.java
@@ -0,0 +1,29 @@
+package nl.utwente.di.OVSoftware;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseTest {
+
+    @Test
+    void getAllPayrates() {
+        assertNotNull(Database.getAllPayrates());
+    }
+
+    @Test
+    void allEmployees() {
+        assertNotNull(Database.allEmployees());
+    }
+
+    @Test
+    void getPayratesSpecificEmployee() {
+        assertNotNull(Database.getPayratesSpecificEmployee(4));
+    }
+
+    @Test
+    void searchEmployees() {
+        assertNotNull(Database.searchEmployees(4,"MaSt"));
+    }
+
+}
