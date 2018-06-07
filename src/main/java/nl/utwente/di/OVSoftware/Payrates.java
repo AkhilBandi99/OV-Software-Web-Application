b4c5e4f45a22c70dffd3bde0ddaa94da0332c49e
Theemok <theesmink@gmail.com>
2018-06-07 11:38:51 +0200

    Finished up payrate integrity checks

--- src/main/java/nl/utwente/di/OVSoftware/Payrates.java
+++ src/main/java/nl/utwente/di/OVSoftware/Payrates.java
@@ -4,7 +4,10 @@ import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
@@ -60,12 +63,66 @@ public class Payrates implements Comparable<Payrates>{
 
 	@Override
 	public int compareTo(Payrates o) {
-		o.getStartDate().compareTo(this.getStartDate());
-		return 0;
+		return startDate.compareTo(o.startDate);
+	}
+	
+	public boolean checkDates() {
+		return startDate.before(endDate);
 	}
 	
 	public String toString() {
 		return getId() + " " + getCost() + " " + getStartDate() + " " + getEndDate();
+	}
+	
+	public static String checkIntegrity(List<Payrates> mainlist) {
+		List<Payrates> head = new ArrayList<>(mainlist);
+		Collections.sort(head);
+		String d = checkPayrate(mainlist);
+		if (d != null) {
+			return d + " Start date after end date";
+		}
+		String i = checkDates(head, mainlist);
+		if (i != null) {
+			return i + " Start date is not next day from previous end date";
+		}
+		return null;
+	}
+	
+	public static String checkPayrate(List<Payrates> head) {
+		int i = 0;
+		while (i < head.size()) {
+			if (!head.get(i).checkDates()) {
+				return i + 1 + ": " + head.get(i).getStartDate() + " " + head.get(i).getEndDate();
+			}
+			i++;
+		}
+		return null;
+	}
+	
+	public static String checkDates(List<Payrates> head, List<Payrates> mainlist) {
+		List<Payrates> list = new ArrayList<Payrates>(head);
+		while(!list.isEmpty()) {
+			int id = list.get(0).getId();
+			List<Payrates> temp = new ArrayList<Payrates>();
+			int i = 0;
+			int i2 = 0;
+			while (i < list.size()) {
+				Payrates item = list.get(i);
+				if (item.getId() == id) {
+					temp.add(item);
+					list.remove(i);
+				} else {
+					i++;
+				}
+			}
+			while (i2 < temp.size() - 1) {
+				if (!temp.get(i2).isNextDate(temp.get(i2 + 1).getStartDate())) {
+					return (mainlist.indexOf(temp.get(i2 + 1)) + 1) + ": " + temp.get(i2).getEndDate() + " " + temp.get(i2 + 1).getStartDate();
+				}
+				i2++;
+			}
+		}
+		return null;
 	}
 	
 }
