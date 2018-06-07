99ddf6bde8ea89d497f1a29594a9ef5d4a93d0cd
leonkukuk <gigalitelk@gmail.com>
2018-06-07 10:17:31 +0200

    Working logout button

--- src/main/java/nl/utwente/di/OVSoftware/LoginResource.java
+++ src/main/java/nl/utwente/di/OVSoftware/LoginResource.java
@@ -1,19 +1,22 @@
 package nl.utwente.di.OVSoftware;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.GET; 
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
 
-@Path("/login/{user}/{pass}")
+@Path("/")
 public class LoginResource {
 	
 	UserMaps validUser = new UserMaps();
 
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/login/{user}/{pass}")
 	public int getClichedMessage(@PathParam("user") String user, @PathParam("pass") String pass, @Context HttpServletRequest r) {
 		if(validUser.findUser(user, pass)) {
 			r.getSession().setAttribute("Timeout", System.currentTimeMillis());
@@ -24,5 +27,11 @@ public class LoginResource {
 		}
 
 	}
-	
+
+	@GET
+	@Path("/logout")
+	public void logOut(@Context HttpServletRequest r, @Context HttpServletResponse resp){
+		r.getSession().invalidate();
+
+	}
 }
